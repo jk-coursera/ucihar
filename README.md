7dd32e53484bb0ded1ae7c0b59ab4f8aee2bd980
jk-coursera <54950943+jk-coursera@users.noreply.github.com>
2019-10-22 22:59:47 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,82 @@
+# Getting and Cleaning Data - Week 4 Project
+
+## About
+
+`run_analysis.R` is an R script that contains functions for working with [UCI's Human Activity Recognition Using Smartphones Dataset](http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones).
+
+## Dependencies
+
+### UCI HAR Dataset
+
+This script assumes that the UCI HAR dataset has been downloaded and extracted, and the "UCI HAR Dataset" folder is accessible in the current working directory within R.
+
+### dplyr
+
+This script is dependent on `dplyr`.
+
+Install `dplyr` using `install.packages`:
+
+    > install.packages("dplyr");
+
+## Functions
+
+### `run_analysis`
+
+Performs the following analysis of the UNI HAR Dataset:
+
+1. Merges the training and the test sets to create one data set.
+2. Extracts only the measurements on the mean and standard deviation for each measurement.
+3. Uses descriptive activity names to name the activities in the data set
+4. Appropriately labels the data set with descriptive variable names.
+5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+Assumes the data is available in the folder `./UCI HAR Dataset` relative to the current working directory.
+
+Parameters:
+
+* None
+
+Returns:
+
+* None
+
+<hr/>
+
+### `ucihar_load`
+
+Loads UCI HAR data into a data frame. It combines the training and the test data sets into one data frame. (The inertial data is not included.)
+
+Parameters:
+* `path` - the path to the extracted "UCI HAR Dataset".
+
+Returns:
+
+A data frame containing the UCI HAR data.
+
+<hr/>
+
+### `ucihar_tidy`
+
+Reduces the data frame loaded with `ucihar_load` to a smaller set of variables and provides nicer names for the variables.
+
+Parameters:
+
+* `ucihar` - the UCI HAR data set loaded from `ucihar_load()`.
+
+Returns:
+
+A simplified version of the provided data frame.
+
+<hr/>
+
+### `ucihar_summarize`
+
+Provides the mean of all observed variables, grouped by Subject ID and Activity Name.
+
+Parameters:
+
+* `ucihar` - the UCI HAR dataset produced by `ucihar_tidy`.
+
+Returns:
+
+A summary data frame of the means of all observed variables, grouped by `Subject.ID` and `Activity.Name` columns.
---
+++ run_analysis.R
@@ -0,0 +1,122 @@
+library(dplyr);
+
+ucihar_load <- function(path)
+{
+  # `path` is the path to the extracted UCI HAR dataset.
+  
+  if(!dir.exists(path))
+  {
+    stop("UCI HAR data directory does not exist: ", path);
+  }
+  
+  # Load pre-processed data
+  
+  # "X_*.txt" contains a large set of 561-element vectors.
+  # Each of the 561 elements have a corresponding name in "features.txt"
+  # X is split between "test/X_test.txt" and "train/X_train.txt"
+  # "Y_*.txt" contains a single value corresponding to labels in activities
+  
+  # ... read feature names
+  features <- read.table(file.path(path, "features.txt"), header=FALSE);
+  activities <- read.table(file.path(path, "activity_labels.txt"), header=FALSE, col.names = c("Activity.ID", "Activity.Name"));
+  
+  # ... read training data
+  train <- read.table(file.path(path, "train", "X_train.txt"), header=FALSE, col.names = features$V2);
+  
+  # ... read training labels, append column to `train`
+  train <- cbind(train, read.table(file.path(path, "train", "Y_train.txt"), header=FALSE, col.names = c("Activity.ID")));
+  
+  # ... read training subjects, append column to `train`
+  train <- cbind(train, read.table(file.path(path, "train", "subject_train.txt"), header=FALSE, col.names = c("Subject.ID")));
+  
+  # ... read testing data
+  test <- read.table(file.path(path, "test", "X_test.txt"), header=FALSE, col.names = features$V2);
+  
+  # ... read testing labels, append column to `test`
+  test <- cbind(test, read.table(file.path(path, "test", "Y_test.txt"), header=FALSE, col.names = c("Activity.ID")));
+  
+  # ... read testing subjects, append column to `test`
+  test <- cbind(test, read.table(file.path(path, "test", "subject_test.txt"), header=FALSE, col.names = c("Subject.ID")));
+  
+  # ... combine the two sets
+  ucihar <- rbind(train, test);
+  
+  # ... clean up intermediate data frames
+  rm(test, train);
+  
+  # Join on "activities"
+  
+  ucihar <- inner_join(ucihar, activities, by = "Activity.ID");
+  
+  # Skipping the inertial signals...
+  # -- not specified in requirements
+  # -- not sure about format of data
+  
+  # Return data set
+  ucihar;
+}
+
+ucihar_tidy <- function(ucihar)
+{
+  # Reduce data to only those columns containing "std" or "mean", and "Subject.ID" and "Activity.Name"
+  
+  features_tidy <- c("Subject.ID", grep("std[.]{3}|mean[.]{3}", names(ucihar), value=TRUE), "Activity.Name");
+  
+  ucihar <- select(ucihar, features_tidy);
+  
+  # Rename columns with descriptive names
+  features_tidy <- lapply(features_tidy, function(feature) {
+    
+    # Time/Freq Domain
+    feature <- gsub("^t", "Time.Domain.", feature);
+    feature <- gsub("^f", "Frequency.Domain.", feature);
+    
+    # Std/Mean
+    feature <- gsub("std[.]{2}", "Std.Dev.", feature);
+    feature <- gsub("mean[.]{2}", "Mean.", feature);
+    
+    # Axis
+    feature <- gsub("[.]?X$", "X.Axis", feature);
+    feature <- gsub("[.]?Y$", "Y.Axis", feature);
+    feature <- gsub("[.]?Z$", "Z.Axis", feature);
+    
+    # Feature
+    feature <- gsub("Body[.]?", "Body.", feature);
+    feature <- gsub("Acc[.]?", "Acceleration.", feature);
+    feature <- gsub("Gyro[.]?", "Gyro.", feature);
+    feature <- gsub("Jerk[.]?", "Jerk.", feature);
+    
+    feature;
+  });
+  
+  # Change column names
+  colnames(ucihar) <- features_tidy;
+  
+  # Return tidied set
+  ucihar;
+}
+
+ucihar_summarize <- function(ucihar)
+{
+  ucihar_grouped <- group_by(ucihar, "Subject.ID", "Activity.Name");
+  
+  features_to_summarize <- names(select(ucihar, -"Subject.ID", -"Activity.Name"))
+  
+  summarize_at(ucihar_grouped, features_to_summarize, mean, na.rm = TRUE);
+}
+
+run_analysis <- function()
+{ 
+  # You should create one R script called run_analysis.R that does the following:
+  
+  # 1. Merges the training and the test sets to create one data set.
+  ucihar <- ucihar_load("./UCI HAR Dataset");
+  
+  # 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+  # 3. Uses descriptive activity names to name the activities in the data set
+  # 4. Appropriately labels the data set with descriptive variable names.
+  ucihar <- ucihar_tidy(ucihar);
+  
+  # 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+  ucihar_summarize(ucihar));
+}
